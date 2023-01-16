86d5e452df125f0c5d3fef789051c2b1cef94173
Денис Богомолов <31684886+den3011den@users.noreply.github.com>
2023-01-17 02:07:47 +0700

    Реализация ProductService. Продолжение работы с frontend продуктов.

--- Mango.Web/Services/IServices/IProductService.cs
+++ Mango.Web/Services/IServices/IProductService.cs
@@ -3,10 +3,10 @@ using System.Threading.Tasks;
 
 namespace Mango.Web.Services.IServices
 {
-    public interface IProductService
+    public interface IProductService : IBaseService
     {
         Task<T> GetAllProductsAsync<T>();
-        Task<T> GetAllProductByIdAsync<T>(int id);
+        Task<T> GetProductByIdAsync<T>(int id);
         Task<T> CreateProductAsync<T>(ProductDTO productDTO);
         Task<T> UpdateProductAsync<T>(ProductDTO productDTO);
         Task<T> DeleteProductAsync<T>(int id);
