78f3c6e05cbdac93593875e0c4c7ac1ffb2809da
Денис Богомолов <31684886+den3011den@users.noreply.github.com>
2023-01-16 20:10:04 +0700

    Добавление Product, ProductDto, репозитория и Automapper для продуктов.

---
+++ Mango.Services.ProductAPI/Repository/IProductRepository.cs
@@ -0,0 +1,15 @@
+﻿using Mango.Services.ProductAPI.Models.Dto;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Mango.Services.ProductAPI.Repository
+{
+    public interface IProductRepository
+    {
+        Task<IEnumerable<ProductDTO>> GetProducts();
+        Task<ProductDTO> GetProductById(int productId);
+        Task<ProductDTO> CreateUpdateProduct(ProductDTO productDto);
+        Task<bool> DeleteProduct(int productId);
+    }
+}
