d59599a2be9021c2edc420dcd138c92504103155
Денис Богомолов <31684886+den3011den@users.noreply.github.com>
2023-01-17 01:27:22 +0700

    Реализация универсального класса вызова API и получения ответа BaseService

--- Mango.Web/Services/BaseService.cs
+++ Mango.Web/Services/BaseService.cs
@@ -1,21 +1,78 @@
 ﻿using Mango.Web.Models;
 using Mango.Web.Services.IServices;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace Mango.Web.Services
 {
     public class BaseService : IBaseService
     {
-        public ResponseDTO responseModel { get => throw new System.NotImplementedException(); set => throw new System.NotImplementedException(); }
+        public ResponseDTO responseModel { get; set; }
+        public IHttpClientFactory httpClient { get; set; }
+
+
+        public BaseService(IHttpClientFactory httpClient)
+        {
+            this.responseModel= new ResponseDTO();
+            this.httpClient = httpClient;
+        }
+
+
+        public async Task<T> SendAsync<T>(ApiRequest apiRequest)
+        {
+            try
+            {
+                var client = httpClient.CreateClient("MangoAPI");
+                HttpRequestMessage message = new HttpRequestMessage();
+                message.Headers.Add("Accept", "application/json");
+                message.RequestUri = new Uri(apiRequest.ApiUrl);
+                client.DefaultRequestHeaders.Clear();
+                if (apiRequest.Data != null)
+                {
+                    message.Content = new StringContent(JsonConvert.SerializeObject(apiRequest.Data), Encoding.UTF8, "application/json");
+                }
+                HttpResponseMessage apiResponse = null;
+                switch(apiRequest.ApiType)
+                {
+                    case SD.ApiType.POST:
+                        message.Method = HttpMethod.Post;
+                        break;
+                    case SD.ApiType.PUT:
+                        message.Method = HttpMethod.Put;
+                        break;
+                    case SD.ApiType.DELETE:
+                        message.Method = HttpMethod.Delete;
+                        break;
+                    default:
+                        message.Method = HttpMethod.Get;
+                        break;
+                }
+                apiResponse = await client.SendAsync(message);
+                var apiContent = await apiResponse.Content.ReadAsStringAsync();
+                var apiResponseDto = JsonConvert.DeserializeObject<T>(apiContent);
+                return apiResponseDto;
+            }
+            catch(Exception e)
+            {
+                var dto = new ResponseDTO
+                {
+                    DisplayMessage = "Error",
+                    ErrorMessages = new List<string> { Convert.ToString(e.Message) },
+                    IsSuccess = false
+                };
+                var res = JsonConvert.SerializeObject(dto);
+                var apiResponseDTO = JsonConvert.DeserializeObject<T>(res);
+                return apiResponseDTO;
+            }
+        }
 
         public void Dispose()
         {
-            throw new System.NotImplementedException();
-        }
-
-        public Task<T> SendAsync<T>(ApiRequest apiRequest)
-        {
-            throw new System.NotImplementedException();
+            GC.SuppressFinalize(true);
         }
     }
 }
