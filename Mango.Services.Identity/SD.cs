ffa1f3bf7dbc209cd0fca5c9037e1ae02b51292a
Денис Богомолов <31684886+den3011den@users.noreply.github.com>
2023-02-01 21:42:20 +0700

    Настройка логина в сисему через Web-форму с авторизацией через Identity Server.

--- Mango.Services.Identity/SD.cs
+++ Mango.Services.Identity/SD.cs
@@ -20,7 +20,7 @@ namespace Mango.Services.Identity
         public static IEnumerable<ApiScope> ApiScopes =>
             new List<ApiScope>
             {
-                new ApiScope("Mango", "Mango Server"),
+                new ApiScope("mango", "Mango Server"),
                 new ApiScope(name: "read", displayName: "Read your data."),
                 new ApiScope(name: "write", displayName: "Write your data."),
                 new ApiScope(name: "delete", displayName: "Delete your data.")
