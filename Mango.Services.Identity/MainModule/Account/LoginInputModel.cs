ae73383418c8c22a98b6ba80f759581fc5ac6592
Денис Богомолов <31684886+den3011den@users.noreply.github.com>
2023-02-01 23:37:36 +0700

    Добавлена возможность регистрации нового пользователя

--- Mango.Services.Identity/MainModule/Account/LoginInputModel.cs
+++ Mango.Services.Identity/MainModule/Account/LoginInputModel.cs
@@ -9,8 +9,11 @@ namespace IdentityServerHost.Quickstart.UI
     public class LoginInputModel
     {
         [Required]
+        [Display(Name = "Имя пользователя")]
         public string Username { get; set; }
+
         [Required]
+        [Display(Name = "Пароль")]
         public string Password { get; set; }
         public bool RememberLogin { get; set; }
         public string ReturnUrl { get; set; }
