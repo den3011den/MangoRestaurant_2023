30e03b9ab6b683f9213cb7ce0d5d63870bb13a37
Денис Богомолов <31684886+den3011den@users.noreply.github.com>
2023-02-04 00:34:32 +0700

    Настройка mappingconfig для ShoppingCartAPI

--- Mango.Services.ShoppingCartAPI/MappingConfig.cs
+++ Mango.Services.ShoppingCartAPI/MappingConfig.cs
@@ -1,4 +1,6 @@
 ﻿using AutoMapper;
+using Mango.Services.ShoppingCartAPI.Models;
+using Mango.Services.ShoppingCartAPI.Models.Dto;
 
 namespace Mango.Services.ShoppingCartAPI
 {
@@ -8,8 +10,10 @@ namespace Mango.Services.ShoppingCartAPI
         {
             var mappingConfig = new MapperConfiguration(config =>
             {
-                //config.CreateMap<ProductDTO, Product>();
-                //config.CreateMap<Product, ProductDTO>();
+                config.CreateMap<ProductDto, Product>().ReverseMap();
+                config.CreateMap<CartHeader, CartHeaderDto>().ReverseMap();
+                config.CreateMap<CartDetails, CartDetailsDto>().ReverseMap();
+                config.CreateMap<Cart, CartDto>().ReverseMap();
             });
             return mappingConfig;
         }
