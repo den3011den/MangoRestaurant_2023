d730457a864784e6454b30e53ce6b836413c9d1a
Денис Богомолов <31684886+den3011den@users.noreply.github.com>
2023-02-11 00:59:12 +0700

    Изменён способ получения пути к домашней папке для считывания сертификата, используемого для публикации сообщений в брокере сообщений Kafka

--- Mango.MessageBus/AzureServiceBusMessageBus.cs
+++ Mango.MessageBus/AzureServiceBusMessageBus.cs
@@ -12,43 +12,50 @@ namespace Mango.MessageBus
     // на самом деле Yandex Cloud - Managed Service for Apache Kafka
     public class AzureServiceBusMessageBus : IMessageBus
     {
-                   
-        private string HOST = "rc1a-s2ka732h0etig7b0.mdb.yandexcloud.net:9091";
-        private string CA_FILE = @"C:\Users\bds\.kafka\YandexCA.crt";
+
+        private string CA_FILE = (Environment.OSVersion.Platform == PlatformID.Unix ||
+                  Environment.OSVersion.Platform == PlatformID.MacOSX)
+                  ? Environment.GetEnvironmentVariable("HOME")
+                  : Environment.ExpandEnvironmentVariables("%HOMEDRIVE%%HOMEPATH%") + "\\.kafka\\YandexCA.crt";
+         private string HOST = "rc1a-s2ka732h0etig7b0.mdb.yandexcloud.net:9091";
+
+        //private string CA_FILE = @"C:\Users\bds\.kafka\YandexCA.crt";
         //var ttt = System.Environment.SpecialFolder..GetEnvironmentVariable("HOME") + "YandexCA.crt";
         //
         public async Task PublishMessage(BaseMessage message, string topicName, string publishUserName, string publishUserPass)
         {
+
+
             var producerConfig = new ProducerConfig(
-                    new Dictionary<string, string>{
+                        new Dictionary<string, string>{
                         {"bootstrap.servers", HOST},
                         {"security.protocol", "SASL_SSL"},
                         {"ssl.ca.location", CA_FILE},
                         {"sasl.mechanisms", "SCRAM-SHA-512"},
                         {"sasl.username", publishUserName},
                         {"sasl.password", publishUserPass}
-                        });
+                            });
 
             var jsonMessage = JsonConvert.SerializeObject(message);
             var finalMessage = jsonMessage; //Encoding.UTF8.GetBytes(jsonMessage);
 
             var producer = new ProducerBuilder<string, string>(producerConfig).Build();
-            
+
             await producer.ProduceAsync(topicName, new Message<string, string> { Key = Guid.NewGuid().ToString(), Value = finalMessage }
-            //,
-            //    (deliveryReport) =>
-            //        {
-            //            if (deliveryReport.Error.Code != ErrorCode.NoError)
-            //            {
-            //                Console.WriteLine($"Failed to deliver message: {deliveryReport.Error.Reason}");
-            //            }
-            //            else
-            //            {
-            //                Console.WriteLine($"Produced message to: {deliveryReport.TopicPartitionOffset}");
-            //        }
-            //        }
-                    
-                    );        
+                    //,
+                    //    (deliveryReport) =>
+                    //        {
+                    //            if (deliveryReport.Error.Code != ErrorCode.NoError)
+                    //            {
+                    //                Console.WriteLine($"Failed to deliver message: {deliveryReport.Error.Reason}");
+                    //            }
+                    //            else
+                    //            {
+                    //                Console.WriteLine($"Produced message to: {deliveryReport.TopicPartitionOffset}");
+                    //        }
+                    //        }
+
+                    );
             producer.Flush(TimeSpan.FromSeconds(10));
             //producer.Dispose();
 
